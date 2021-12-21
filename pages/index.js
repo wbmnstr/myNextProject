f1993f58d4e8ff595af0006c30effe180058efa6
wbmnstr <fatihsaridag@gmail.com>
2021-12-21 15:07:19 +0300

    kurulum
    ilk kurulum ayarları yapıldı

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Webdev Newz</title>
+        <meta name='keywords' content='web development' />
+      </Head>
+      <h1>Welcome to Home</h1>
+    </div>
+  )
+}
